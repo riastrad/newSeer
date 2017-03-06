c73ca4c6fe772cef7d7dbe5b229e8f35c1852cda
Josh <josherb4@gmail.com>
2017-03-06 15:53:45 -0500

    some basic wrangling code

---
+++ driver/cleaner.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+#
+# @Author: Josh Erb <josh.erb>
+# @Date:   06-Mar-2017 14:03
+# @Email:  josh.erb@excella.com
+# @Last modified by:   josh.erb
+# @Last modified time: 06-Mar-2017 14:03
+
+"""
+Quick script to clean up .csv data that was pulled from RSS feeds (ref: eater.py)
+
+Will clean files in place and leave them in the ./data folder - after which the
+'dum.py' file will insert all data into a local PostgreSQL database for safekeeping (and storage purposes).
+"""
+
+#######################################
+#   IMPORTS
+#######################################
+
+import os
+import ftfy
+from glob import glob
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+
+#######################################
+#   CONSTANTS
+#######################################
+
+data_path = os.getcwd() + '/data/'
+data_files = glob(data_path + '*.csv')
+
+#######################################
+#   FUNCTIONS
+#######################################
+
+def csv_as_df(file_path):
+    """
+    Using a file path string as an argument, opens a csv as a dataframe with
+    no headers and returns that dataframe.
+    """
+
+    with open(file_path, 'r') as f:
+        df = pd.read_csv(file_path)
+
+    return df
+
+def clean_title(df, col=4):
+    """
+    A function that takes an RSS feed data frame as its argument and returns a
+    dataframe with the regex errors cleaned up as best as can be managed.
+    """
+    # Quickly iterate over the dataframe and clean each title value
+    for i, row in df.itterows():
+        clean = ftfy.fix_text(df.iloc[i, col])
+        df.set_value(i, 5, clean)
+
+    return df
+
+def clean_desc(df, col=0):
+    """
+    A function that takes an RSS feed data frame as its argument and returns a
+    dataframe with the html removed from the description column, as best as
+    possible given the state of some of the RSS pulls.
+
+    (I'm looking at you, BuzzFeed)
+    """
+
+    # similar iteration to clean_title, but leveraging the prebuilt functions that
+    # can be run on BeautifulSoup objects to parse html text
+    for i, row in df.iterrows():
+        clean = BeautifulSoup(df.iloc[i, col]).text
+        df.set_value(i, 6, clean)
+
+    return df
+
+def clean_save(df, file_name, path=data_path):
+    """
+    Runs the cleaning functions on the text columns of an RSS feed dataframe. A
+    file name needs to be provided in order to save properly.
+
+    Note that the final output is saved as a tsv (tab seperated) file in order
+    to avoid any unintended consequences with free text commas.
+    """
+    # add a title name onto the path variable
+    new_path = data_path + file_name + '_clean.tsv'
+
+    # run each predefined function on a given data frame
+    df = clean_title(df)
+    df = clean_desc(df)
+
+    # drop the unneaded, 'dirty' columns (using their indexes)
+    df.drop([0,4], axis=1, inplace=True)
+
+    # save the file as a .tsv
+    pd.to_csv(new_path, sep='\t', header=['pull_ts', 'pubdate', 'publication', 'title', 'description'])
+
+    return
+
+def main():
+    """
+    Primary execution function.
+    """
+
+    for csv in tqdm(data_path): # the tqdm() function call is only there to show progress, because I imagine running this on a large amount of large files
+        name = csv[:-4] # using the filename to help generate a new file name
+        df = csv_as_df(csv)
+        clean_save(df, name)
+
+    return
+
+#######################################
+#   EXECUTION
+#######################################
+
+if __name__ == '__main__':
+    main()
---
+++ driver/dum.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+#
+# @Author: Josh Erb <josh.erb>
+# @Date:   06-Mar-2017 15:03
+# @Email:  josh.erb@excella.com
+# @Last modified by:   josh.erb
+# @Last modified time: 06-Mar-2017 15:03
+
+"""
+Quick script to glob up a bunch of tsv files and insert them into a local
+PostgreSQL instance using psycopg2.
+
+Will only work if files have been previously cleaned using the cleaner.py script
+once this is completed and results have been validated, feel free to delete
+raw .csv files.
+"""
+
+#######################################
+#   IMPORTS
+#######################################
+
+import psycopg2
+import pandas as pd
+from cleaner import csv_as_df
+
+#######################################
+#   CONSTANTS
+#######################################
+
+data_path = os.getcwd() + '/data/'
+data_files = glob(data_path + '*.tsv')
+
+#######################################
+#   FUNCTIONS
+#######################################
+
+def insert_data(df, conn):
+    """
+    Uses pandas and psycopg2 to insert a dataframe into a pre-existing SQL
+    database table on a local PostgreSQL instance.
+
+    Note that a connection must have been opened and passed to this function in
+    order for it to run correctly.
+    """
+    pass
+
+def main():
+    """
+    Primary execution function.
+    """
+    # initialize the db connection & cursor
+    conn = psycopg2.connect("dbname=seer user=josh.erb")
+    cur = conn.cursor()
+
+    # add manipulation code here
+    for data in data_files:
+        # right here
+
+    # close out connection
+    cur.close()
+    conn.close()
+
+    return
+
+#######################################
+#   EXECUTION
+#######################################
+
+if __name__ == '__main__':
+    main()
